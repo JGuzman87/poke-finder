6a2e935cb1c38b47c667e0538c6ed9055885213f
JGuzman87 <jguzman913@gmail.com>
2025-04-24 20:37:18 -0400

    added error and and Alert component and transition

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,32 @@
-"use client"
+"use client";
 import Card from "@/components/Card";
+import Alert from "@/components/Alert";
 import { useState, useEffect } from "react";
 
 import Nav from "../components/Nav";
 
 export default function Home() {
-
   const [pokeInfo, setPokeInfo] = useState({});
   const [pokeName, setPokeName] = useState("");
   const [sprite, setSprite] = useState({});
   const [abilities, setAbilities] = useState(null);
+  const [error, setError] = useState();
 
   useEffect(() => {
-
-    const pokemonid = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-    const randomid = pokemonid[Math.floor(Math.random() * pokemonid.length )];
+    const pokemonid = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ];
+    const randomid = pokemonid[Math.floor(Math.random() * pokemonid.length)];
     const fetchRandom = async () => {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomid}`);
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${randomid}`
+      );
       const randomData = await response.json();
       setPokeInfo(randomData);
       setSprite(randomData.sprites);
-      setAbilities(randomData.abilities)
-
-    }
-    fetchRandom()
+      setAbilities(randomData.abilities);
+    };
+    fetchRandom();
   }, []);
 
   const fetchPokemon = async () => {
@@ -32,19 +35,21 @@ export default function Home() {
         `https://pokeapi.co/api/v2/pokemon/${pokeName}`
       );
       const pokeData = await response.json();
+      setError(null);
       setPokeInfo(pokeData);
       setSprite(pokeData.sprites);
       console.log(pokeData);
       // console.log(pokeData.abilities[0].ability.name);
       setAbilities(pokeData.abilities);
     } catch (error) {
-      console.error("Error fetching Pokemon data:", error);
+      setError("Not a valid pokemon name or id.");
+      console.error(error)
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+ 
     fetchPokemon();
   };
   const handleChange = (e) => {
@@ -54,14 +59,16 @@ export default function Home() {
     }
   };
 
-
-
   return (
     <main className="grid grid-cols-1 gap-4 w-full p-4 md:grid-cols-3 md:max-w-full">
-
       <Nav onSubmit={handleSubmit} value={pokeName} onChange={handleChange} />
-   
-
+      <div
+        className={`transition-all duration-500 ease-in-out ${
+          error ? "opacity-100 scale-100" : "opacity-0 scale-95"
+        }`}
+      >
+        {error && <Alert error={error} />}
+      </div>
       {pokeInfo.name ? (
         <Card
           name={pokeInfo.name}
@@ -75,4 +82,5 @@ export default function Home() {
 
       {pokeInfo.moves ? <Card moves={pokeInfo.moves} /> : ""}
     </main>
-  )};
+  );
+}
